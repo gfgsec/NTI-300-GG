1309fb3675d64e0b815287ca757bcfa53d742e4d
grantypantyyy <grant.grismore@seattlecolleges.edu>
2016-10-16 16:23:05 -0700

    Update install-apache.sh

--- install-apache.sh
+++ install-apache.sh
@@ -9,4 +9,8 @@ sudo systemctl enable httpd.service
 echo "starting apache server"
 sudo systemctl start httpd.service
 
-echo "If you open the security settings for port 80 on your server, you should see the apache start page"
+echo "cloning branch from Grants github"
+sudo git clone https://github.com/grantypantyyy/NTI-300-GG/tree/website
+
+echo "Bamboozeling an html file"
+sudo mv NTI-300-GG/tree/website/grantweb.html 
