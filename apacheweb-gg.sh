818af73fa8d8fcee20d9050786ddc69907368682
grantypantyyy <grant.grismore@seattlecolleges.edu>
2016-10-16 16:45:01 -0700

    Update apacheweb-gg.sh

--- apacheweb-gg.sh
+++ apacheweb-gg.sh
@@ -10,7 +10,7 @@ echo "starting apache server"
 sudo systemctl start httpd.service
 
 echo "cloning branch from Grants github"
-sudo git clone https://github.com/grantypantyyy/NTI-300-GG/tree/website
+sudo git clone https://github.com/grantypantyyy/NTI-300-GG.git
 
 echo "Bamboozeling an html file"
 sudo mv NTI-300-GG/tree/website/grantweb.html /var/www/html
