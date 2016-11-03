157301d3cce6b7b82be658e3769595ee6bb1307c
grantypantyyy <grant.grismore@seattlecolleges.edu>
2016-11-02 21:15:36 -0700

    Update cron1.sh

--- cron1.sh
+++ cron1.sh
@@ -17,3 +17,13 @@ fi
 ##Crontab entry to run script every 30 minutes:
 
 30 * * * * /home/ec2-user/server_alert3.sh | mail -s "Server Alert" grant.grismore@seattlecolleges.edu
+
+# ┌───────────── min (0 - 59)
+# │ ┌────────────── hour (0 - 23)
+# │ │ ┌─────────────── day of month (1 - 31)
+# │ │ │ ┌──────────────── month (1 - 12)
+# │ │ │ │ ┌───────────────── day of week (0 - 6) (0 to 6 are Sunday to
+# │ │ │ │ │                  Saturday, or use names; 7 is also Sunday)
+# │ │ │ │ │
+# │ │ │ │ │
+# * * * * *  command to execute
