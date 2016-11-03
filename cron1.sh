0e6ef7105ffe388ae003e7733e23b30a0e92061e
grantypantyyy <grant.grismore@seattlecolleges.edu>
2016-11-02 20:43:32 -0700

    Update cron1.sh

--- cron1.sh
+++ cron1.sh
@@ -7,5 +7,5 @@ echo "number of users loged in is $users"
 if [ "$users" -gt "0" ]; then
     echo "there's more than one user logged in"
   else
-    echo "There are no users logged in"
+    echo "There are no users logged in" | mail -s "There are no users logged into your Red Hat Instance" grant.grismore@seattlecolleges.edu
 fi
