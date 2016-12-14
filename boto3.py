cc71156e8e00c94d0fe3b7bea64611fc2bb1fb03
grantypantyyy <grant.grismore@seattlecolleges.edu>
2016-12-13 19:34:01 -0800

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -39,19 +39,19 @@ hello()
   
 def tree_install():
   print('Installing tree')
-  os.system('sudo yum -y install tree')
+  os.system('yum -y install tree')
   
 tree_install()
 
 def apache_install():
   print('Installing Apache Web Server')
-  os.system('sudo yum -y install httpd')
+  os.system('yum -y install httpd')
   
   print('Starting http service')
-  os.system('sudo systemctl enable httpd.service')
+  os.system('systemctl enable httpd.service')
   
   print('Starting apache server')
-  os.system('sudo systemctl start httpd.service')
+  os.system('systemctl start httpd.service')
   
   print('IMPORTANT! Create an inbound security rule to open port 80 on the server')
   
@@ -59,7 +59,7 @@ apache_install()
 
 def clone_repo():
   print('installing git')
-  os.system('sudo yum -y install git')
+  os.system('yum -y install git')
   
   print('cloning Grants github repo')
   os.system('git clone https://github.com/grantypantyyy/NTI-300-GG.git')
@@ -68,11 +68,11 @@ clone_repo()
 
 def publish_site():
   print('Publishing Website')
-  os.system('sudo cp NTI-300-GG/webgrant.html /var/www/html')
+  os.system('cp NTI-300-GG/webgrant.html /var/www/html')
   print('adjusting permisions')
-  os.system('sudo chmod 644 /var/www/html/webgrant.html')
-  os.system('sudo setenforce 0')
-  os.system('sudo service httpd restart')
+  os.system('chmod 644 /var/www/html/webgrant.html')
+  os.system('setenforce 0')
+  os.system('service httpd restart')
   
 publish_site()
 
@@ -87,13 +87,13 @@ django_install()
 
 def mailx():
     print('Installing mailx')
-    os.system('sudo yum -y install mailx')
+    os.system('yum -y install mailx')
 
 mailx()
 
 def crontab():
     print('Creating crontab entry for Server Alert emails every 30 minutes.')
-    os.system('sudo chmod +x /home/ec2-user/NTI-300-GG/cron2.sh')
+    os.system('chmod +x /home/ec2-user/NTI-300-GG/cron2.sh')
     os.system('(crontab -l 2>/dev/null; echo "0,30 * * * * /home/ec2-user/NTI-300-GG/cron2.sh | mail -s \"Server Alert\" grantgrismore@outlook.com") | crontab - ')
     os.system('crontab -l')
 
@@ -101,22 +101,22 @@ crontab()
 
 def update_kernel():
   print('Updating kernal')
-  os.system('sudo yum clean all && sudo yum update kernel -y')
+  os.system('yum clean all && sudo yum update kernel -y')
   print('Done!')
   print('Verifying dirty cow patch')
-  os.system('sudo rpm -q --changelog kernel | grep CVE-2016-5195')
+  os.system('rpm -q --changelog kernel | grep CVE-2016-5195')
   
 update_kernel()
 
 def awscli():
     print('Installing the AWS CLI')
-    os.system('sudo pip install -y awscli')
+    os.system('pip install -y awscli')
 
 awscli()
 
 def boto3():
     print('Installing Boto3')
-    os.system('sudo pip install -y boto3')
+    os.system('pip install -y boto3')
     print('Install Script Complete! AWS RHEL Server ready to use.')
   
 boto3()
