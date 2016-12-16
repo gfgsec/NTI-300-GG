06f7015ab7b01c7dd08e783f7512468f3534f804
grantypantyyy <grant.grismore@seattlecolleges.edu>
2016-12-15 18:21:35 -0800

    Update boto3yo.py

--- boto3yo.py
+++ boto3yo.py
@@ -9,7 +9,7 @@ client = boto3.client('ec2')
 
 amazon_image = 'ami-6f68cf0f'                                       # This will launch a red hat instance
 amazon_instance = 't2.micro'                                        # we've been working with micro's, if you use Amizon Linux, you could launch a nono
-amazon_pem_key = 'NTI300.pem'                    # the name of the key/pem file you would like to use to access this machine
+amazon_pem_key = 'NTI300'                    # the name of the key/pem file you would like to use to access this machine
 firewall_profiles = ['default']                             # the security group name(s) you would like to use, remember, this is your firewall, make sure the ports you want open are open
 
 print(amazon_image)
